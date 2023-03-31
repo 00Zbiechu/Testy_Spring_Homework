fadc77ede3a25f5d6fc42ffdfe2ae43becc6cf8d
Windows <00Zbiewski@gmail.com>
2023-03-31 21:26:12 +0200

    Zmiana Person Mappera z własnego mappera na Mapstructowego

--- src/main/java/pl/testy/zadanie/testy_spring_homework/mapper/PersonMapper.java
+++ src/main/java/pl/testy/zadanie/testy_spring_homework/mapper/PersonMapper.java
@@ -2,6 +2,7 @@ package pl.testy.zadanie.testy_spring_homework.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import pl.testy.zadanie.testy_spring_homework.entity.Address;
 import pl.testy.zadanie.testy_spring_homework.entity.Person;
@@ -13,26 +14,39 @@ import java.util.stream.Collectors;
 
 
 
-@Mapper(uses = {PersonMapper.class,AddressMapper.class}, componentModel = "spring")
+@Mapper(uses = {PersonMapper.class}, componentModel = "spring")
 public interface PersonMapper extends BaseMapper<Person, PersonDTO> {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    default PersonDTO toDTO(Person person){
-        return PersonDTO.builder()
-                .id(person.getId())
-                .firstName(person.getFirstName())
-                .lastName(person.getLastName())
-                .birthDate(person.getBirthDate())
-                .defaultAddress(person.getAddress().stream()
-                        .filter(Address::isDefaultAddress)
-                        .distinct().map(AddressMapper.INSTANCE::toDTO)
-                        .toList().get(0))
-                .addresses(person.getAddress().stream()
-                        .filter(e-> !e.isDefaultAddress())
-                        .map(AddressMapper.INSTANCE::toDTO)
-                        .collect(Collectors.toList()))
-                .build();
+    @Mapping(source="id", target="id")
+    @Mapping(source="firstName", target="firstName")
+    @Mapping(source="lastName", target="lastName")
+    @Mapping(source="birthDate", target="birthDate")
+    @Mapping(source="address",target="defaultAddress", qualifiedByName = "addressToDefaultAddress")
+    @Mapping(source="address",target="addresses", qualifiedByName = "noAddressToDefaultAddress")
+    PersonDTO toDTO(Person person);
+
+
+    @Named("addressToDefaultAddress")
+    default AddressDTO addressToDefaultAddress(List<Address> address){
+
+        return address.stream()
+                .filter(Address::isDefaultAddress)
+                .distinct().map(AddressMapper.INSTANCE::toDTO)
+                .toList().get(0);
+
+    }
+
+
+    @Named("noAddressToDefaultAddress")
+    default List<AddressDTO> noAddressToDefaultAddress(List<Address> address){
+
+        return address.stream()
+                .filter(e-> !e.isDefaultAddress())
+                .map(AddressMapper.INSTANCE::toDTO)
+                .collect(Collectors.toList());
+
     }
 
 }
